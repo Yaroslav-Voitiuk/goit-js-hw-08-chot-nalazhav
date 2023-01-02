a1570c7ad2876166416bd335b45079868e400a1b
Yaroslav-Voitiuk <yaroslavvoitiuk45@gmail.com>
2023-01-02 22:36:49 +0200

    121

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,27 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`,
+  ''
+);
+gallery.insertAdjacentHTML('beforeend', markup);
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
